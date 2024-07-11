f11cee3c72619257febc8199b28cda762f5ce237
ndumi-cloud <ndumigrace71@gmail.com>
2024-07-11 23:24:36 +0300

    fixed bookings issue

--- backend/models/BookingSchema.js
+++ backend/models/BookingSchema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const bookingSchema = new mongoose.Schema(
   {
     doctor: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
       
       required: true,
     },
